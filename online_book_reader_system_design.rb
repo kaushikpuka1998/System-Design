8189a506ff637ec53104074adbc6b02a9cc5da5d
kaushik.ghosh <kaushik.ghosh@intrcity.com>
2023-01-24 23:59:08 +0530

    All class and inheritence and multiple class call instance creation done

---
+++ online_book_reader_system_design.rb
@@ -0,0 +1,162 @@
+class OnlineReaderSystem
+
+    @Library
+    @OnlineManager
+    @Display
+    @Active_users
+    
+    def initialize
+        @Library = Library.new
+        @Display = Display.new
+        @Active_users = Hash.new
+    end
+
+    def getLibrary
+        @Library
+    end
+
+    def getDisplay
+        @Display
+    end
+
+    def get_active_users
+        @Active_users
+    end
+
+    def add_books(book)
+
+        @Library.add_books_to_the_list(book)
+    end
+
+    def adding_booking_to_user(user_id,book_id)
+        @Library.allocate_book_to_user(user_id,book_id)  
+    end
+
+    
+    
+end
+
+
+class Display
+
+end
+
+class User
+    
+    @id
+    @name
+
+    def initialize(id,name)
+        @id =id
+        @name = name
+    end
+
+    def get_user_name
+        @name
+    end
+
+    attr_accessor :id
+end
+
+class Library
+
+
+    @list_of_books
+    def initialize
+        @list_of_books = Hash.new
+    end
+
+    def add_books_to_the_list(book)
+
+        if @list_of_books[book.book_id].nil?
+            @list_of_books[book.book_id] = book
+
+            puts "=====#{book.get_book_name} Added in the Library===="
+        else
+            puts "Same Book ID Found"
+        end    
+    end
+
+    def allocate_book_to_user(user_id,book_id)
+        if !@list_of_books[book_id].nil?
+            if @list_of_books[book_id].get_alloc_user
+                puts "Sorry!!! #{@list_of_books[book_id].get_book_name} allocated to UserID: #{@list_of_books[book_id].get_alloc_user}"
+            else
+                @list_of_books[book_id].set_alloc_user(user_id)
+                puts "Booking ID: #{book_id} is Allocated to User ID: #{user_id} "
+            end
+        else
+            puts "Sorry!!! The book ID: #{book_id} is not present in the Library"
+        end
+    end
+
+    def books_list
+        @list_of_books
+    end
+
+end
+
+class Book
+    
+    @book_id
+    @book_name
+    @avaiable
+    @aloc_user
+   def initialize(id,name,avaiable)
+        @book_id = id
+        @book_name = name
+        @avaiable = avaiable
+        @aloc_user = -15
+   end
+
+   def get_book_name
+        @book_name
+   end
+
+   def get_availability
+        @avaiable
+   end
+   
+   def get_alloc_user
+        @aloc_user
+   end
+
+   def set_alloc_user(user_id)
+       @aloc_user = user_id 
+   end
+
+   attr_accessor :book_id
+end
+
+online_reader_system = OnlineReaderSystem.new
+
+algo_book1 = Book.new(1,"Algo_book",1)
+
+algo_book2 = Book.new(2,"Introduction to Java",1)
+
+kaushik = User.new(134,"Kaushik Ghosh")
+sanglap = User.new(135,"sanglap Dey")
+
+
+# online_reader_system.get
+
+#puts (algo_book1.book_id)
+
+online_reader_system.getLibrary.add_books_to_the_list(algo_book1)
+
+online_reader_system.getLibrary.add_books_to_the_list(algo_book2)
+
+
+puts online_reader_system.adding_booking_to_user(134,1);
+puts online_reader_system.adding_booking_to_user(135,4);
+
+
+
+# value = {1 => "PAPC"}
+
+# value.map{ |f,s|
+#     @Library[f] = s
+# }
+
+
+
