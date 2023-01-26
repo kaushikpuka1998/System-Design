c5c4be6fde3621a7f55a4a45a4147e7aafbcab76
kaushik.ghosh <kaushik.ghosh@intrcity.com>
2023-01-27 03:12:47 +0530

    condition added by movie finding by city and adding movie and find movie by name

--- Movie_booking_system/src/Controllers/MovieController.java
+++ Movie_booking_system/src/Controllers/MovieController.java
@@ -3,6 +3,8 @@ package Controllers;
 import Models.City;
 import Models.Movie;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -10,5 +12,35 @@ public class MovieController {
     Map<City, List<Movie>> cityagainstmovie;
     List<Movie> allmovies;
 
+    public MovieController()
+    {
+        cityagainstmovie = new HashMap<>();
+        allmovies = new ArrayList<>();
+    }
+
+    public void addMovie(City city,Movie movie)
+    {
+        allmovies.add(movie);
+        List<Movie> allmoviesmappedwithcity = cityagainstmovie.getOrDefault(city,new ArrayList<>());
+        allmoviesmappedwithcity.add(movie);
+        cityagainstmovie.put(city,allmoviesmappedwithcity);
+    }
+
+    public Movie getMovieByName(String name)
+    {
+        for(Movie movie:allmovies)
+        {
+            if(movie.getName() == name)
+            {
+                return movie;
+            }
+        }
+        return null;
+    }
+
+    public List<Movie> getAllmoviesByCityName(City city)
+    {
+        return cityagainstmovie.get(city);
+    }
 
 }
